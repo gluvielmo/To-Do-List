31ee7aeeda906cadda38026eae6c05d9134a6432
GuiLuv <luvielmo.guilherme@gmail.com>
2020-04-16 20:10:07 -0300

    add local storage

--- todos.js
+++ todos.js
@@ -2,21 +2,23 @@ var list = document.querySelector('.task ul')
 var taskDiv = document.querySelector('.task .addTask')
 var btn = document.querySelector('.task a')
 
-var tasks = [
-    'Study C#',
-    'Workout',
-    'Read a book'
-]
+var tasks = JSON.parse(localStorage.getItem('taskList')) || ['Study JS', 'Workout', 'Read a book'];
 
-function renderDefaultTasks() {
+function renderTasks() {
+    list.innerHTML = '';
+
     for (task of tasks) {
         var taskElement = document.createElement('li')
         var taskText = document.createTextNode(task)
 
-        var newBtn = document.createElement("a")
-        var removeText = document.createTextNode("Remove")
+        var newBtn = document.createElement('a')
+        var removeText = document.createTextNode('Remove')
         newBtn.appendChild(removeText)
         newBtn.setAttribute("class", "remove")
+
+        var pos = tasks.indexOf(task);
+
+        newBtn.setAttribute('onclick', 'removeTask(' + pos + ')');
 
         taskElement.appendChild(taskText)
         taskElement.appendChild(newBtn)
@@ -24,69 +26,52 @@ function renderDefaultTasks() {
     }
 }
 
-renderDefaultTasks()
-
-var remove = document.getElementsByClassName("remove");
-
-for (var i = 0; i < remove.length; i++) {
-    remove[i].onclick = function() {
-        this.parentElement.remove()
-    }
-}
+renderTasks()
 
 function createInput() {
-    var newDiv = document.createElement("div");
-    var newItem = document.createElement("input");
-    newItem.style.width = "100%"
+    var newDiv = document.createElement('div');
+    var newItem = document.createElement('input');
+    newItem.style.width = '100%'
 
     newDiv.appendChild(newItem);
     taskDiv.insertBefore(newDiv, taskDiv.childNodes[0]);
 
-    btn.style.display = "none"
+    btn.style.display = 'none'
 
-    var newBtn = document.createElement("a")
-    var taskText = document.createTextNode("Add new task")
+    var newBtn = document.createElement('a')
+    var taskText = document.createTextNode('Add new task')
     newBtn.appendChild(taskText)
-    newBtn.setAttribute("class", "newBtn")
+    newBtn.setAttribute('class', 'newBtn')
 
     taskDiv.appendChild(newBtn)
 
     newBtn.onclick = () => {
         addtask();
-        newDiv.remove()
-        newBtn.remove()
-        btn.style.display = "inline-block"
+        newDiv.remove();
+        newBtn.remove();
+        btn.style.display = 'inline-block'
     }
 }
 
 function addtask() {
     var inputValue = document.querySelector('.task input').value
     if (inputValue) {
-        var taskElement = document.createElement('li')
-        var taskText = document.createTextNode(inputValue)
-
-        taskElement.appendChild(taskText)
-        list.appendChild(taskElement)
-
-        var newBtn = document.createElement("a")
-        var removeText = document.createTextNode("Remove")
-        newBtn.appendChild(removeText)
-        newBtn.setAttribute("class", "remove")
-
-        taskElement.appendChild(taskText)
-        taskElement.appendChild(newBtn)
-        list.appendChild(taskElement)
+        tasks.push(inputValue)
     }
 
-    var remove = document.getElementsByClassName("remove");
-
-    for (var i = 0; i < remove.length; i++) {
-        remove[i].onclick = function() {
-            this.parentElement.remove()
-        }
-    }
+    renderTasks();
+    saveToStorage();
 }
 
-btn.onclick = () => {
-    createInput();
+btn.onclick = createInput;
+
+function removeTask(pos) {
+    tasks.splice(pos, 1);
+
+    renderTasks();
+    saveToStorage();
+}
+
+function saveToStorage() {
+    localStorage.setItem('taskList', JSON.stringify(tasks))
 }
